af2ef700e9de099c72c22faeaa65186ceb54533f
kagers <ka.gershfeld@gmail.com>
2016-02-23 09:54:03 -0500

    oh stop it you

--- hw02/animate.js
+++ hw02/animate.js
@@ -3,10 +3,12 @@
 var c = document.getElementById("playground");
 var ctx = c.getContext("2d");
 
-var b = document.getElementById("clear");
+var start  = document.getElementById("start");
+var stop  = document.getElementById("stop");
 
 var r = 1;
 var growing = true;
+var requestID;
 
 var clr = function clr() {
     ctx.clearRect(0,0,538,538); // clears everything in the canvas
@@ -28,7 +30,13 @@ var anime = function anime(e) {
     } else if (r > 269) {
         growing = false;
     }
-    window.requestAnimationFrame(anime); // repeat
+    requestID = window.requestAnimationFrame(anime); // repeat
 };
 
-b.addEventListener("click",anime); // button triggers animation
+var no = function no() {
+    console.log(requestID);
+    window.cancelAnimationFrame(requestID);
+}
+
+start.addEventListener("click",anime); // button triggers animation
+stop.addEventListener("click",no); // button triggers animation
