407d7f1106910a2c234c62c8f73bbf045c136b87
kagers <ka.gershfeld@gmail.com>
2016-03-17 10:13:49 -0400

    wg'

--- paul/bar.js
+++ paul/bar.js
@@ -5,3 +5,5 @@ var barUpdate = bar.data(data);
 var barEnter = barUpdate.enter().append("div");
 
 barEnter.style("width",function(d) { return d*10+"px"; });
+barEnter.text(function(d) { return d; });
+
