0a62dd773e51ea779932fba062460807df78685f
kagers <Ka.gershfeld@gmail.com>
2016-03-20 17:24:48 -0400

    hdungo

--- hw05/index.js
+++ hw05/index.js
@@ -1,14 +1,5 @@
 var data = [4,8,15,16,23,42];
 
-/*var chart = d3.select(".chart");
-var bar = d3.selectAll("div");
-var barUpdate = bar.data(data);
-var barEnter = barUpdate.enter().append("div");
-
-barEnter.style("width",function(d) { return d*10+"px"; });
-barEnter.text(function(d) { return d; });
-*/
-
 d3.select(".chart")
   .selectAll("div")
     .data(data)
